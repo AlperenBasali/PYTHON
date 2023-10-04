f9fc2759bc6b3059b48a027632574c6e19cee22a
AlperenBasali <alperenbasali3@gmail.com>
2023-10-04 22:35:11 +0300

    tekrar

---
+++ DERSLER/ders5.py
@@ -0,0 +1,146 @@
+# ! Hesap Makinesi Örneği
+
+# giris = f"""
+# 1 Topla
+# 2 Çıkar
+# 3 Çarp
+# 4 Böl
+# 5 Üssünü Hesapla
+# 6 Karekökünü Hesapla
+# """
+
+# print(giris)
+
+# while True:
+#     islem = input("Yapmak istediğiniz işlemi giriniz. (Çıkış yapmak için q:)")
+
+#     if islem == "q":
+#         print("Çıkış yapılıyor...")
+#         break
+#     elif islem == "1":
+#         sayi1 = int(input("Toplama işlemi için ilk sayıyı giriniz:"))
+#         sayi2 = int(input("Toplama işlemi için ikinci sayıyı giriniz:"))
+#         sonuc = sayi1 + sayi2
+#         print(f"Sonuc: {sonuc}")
+#     elif islem == "2":
+#         sayi1 = int(input("Çıkarma işlemi için ilk sayıyı giriniz:"))
+#         sayi2 = int(input("Çıkarma işlemi için ikinci sayıyı giriniz:"))
+#         sonuc = sayi1 - sayi2
+#         print(f"Sonuc: {sonuc}")
+#     elif islem == "3":
+#         sayi1 = int(input("Çarpma işlemi için ilk sayıyı giriniz:"))
+#         sayi2 = int(input("Çarpma işlemi için ikinci sayıyı giriniz:"))
+#         sonuc = sayi1 * sayi2
+#         print(f"Sonuc: {sonuc}")
+#     elif islem == "4":
+#         sayi1 = int(input("Bölme işlemi için ilk sayıyı giriniz:"))
+#         sayi2 = int(input("Bölme işlemi için ikinci sayıyı giriniz:"))
+#         sonuc = sayi1 / sayi2
+#         print(f"Sonuc: {sonuc}")
+#     elif islem == "5":
+#         sayi1 = int(input("Üs alma işlemi için ilk sayıyı giriniz:"))
+#         sayi2 = float(input("Üs alma işlemi için ikinci sayıyı giriniz:"))
+#         sonuc = sayi1 ** sayi2
+#         print(f"Sonuc: {sonuc}")
+#     elif islem == "6":
+#         sayi1 = int(input("Karekök alma işlemi için ilk sayıyı giriniz:"))
+#         sonuc = sayi1 ** 0.5
+#         print(f"Sonuc: {sonuc}")
+#     else:
+#         print("Geçerli bir işlem giriniz")
+
+
+# ! ATM UYGULAMASI
+# def atm():
+#     hesabım = "Furkan"
+#     sifrem = "1234"
+#     bakiye = 5000
+#     borc = 3000
+#     hak = 3
+
+#     while True:
+#         hak = hak - 1
+#         print(f"Kalan hakkınız {hak}")
+#         kullanici = input("Kullanıcı adınızı giriniz:")
+#         sifre = input("Şifrenizi giriniz:")
+#         if kullanici == hesabım and sifre == sifrem:
+#             print(f"Bakiyeniz : {bakiye}₺")
+#             while True:
+#                 print(f"""
+
+# Hoşgeldiniz. Yapabileceğiniz İşlemler:
+# 1-Para Çekme
+# 2-Para Yatırma
+# 3-Para Gönderme
+# 4-Şifre Değiştirme
+# 5-Bakiye Sorgulama
+# 6-Borç Sorgulama
+# 7-Çıkış                     
+                      
+# """)
+#                 islem = int(input("Yapacağınız işlemi giriniz:"))
+#                 if islem == 1:
+#                     print(f"Bakiyeniz : {bakiye}₺")
+#                     cek = int(input("Çekmek istediğiniz tutarı giriniz:"))
+#                     if bakiye > cek:
+#                         bakiye = bakiye - cek
+#                         print(f"Para çekme işleminiz başarıyla gerçekleştirildi. Güncel bakiyeniz : {bakiye}₺ ")
+#                     else:
+#                         print("Bakiyeniz yetersiz")
+
+#                 elif islem == 2:
+#                     yatir = int(input("Yatırmak istediğiniz tutarı giriniz:"))
+#                     bakiye = bakiye + yatir
+#                     print(f"Para yatırma işleminiz başarılı. Güncel bakiyeniz : {bakiye}₺")
+                
+#                 elif islem == 3:
+#                     print(f"Güncel bakiye {bakiye}₺")
+#                     gönder = int(input("Gönderilecek tutarı giriniz:"))
+#                     if bakiye >= gönder:
+#                         bakiye -= gönder
+#                         print(f"İşlem başarılı. Kalan bakiyeniz: {bakiye}₺")
+#                     else:
+#                         print("Bakiyeniz yetersiz")
+                
+#                 elif islem == 4:
+#                     while True:
+#                         mevcutSifre = input("Mevcut şifrenizi giriniz:")
+#                         if mevcutSifre == sifrem:
+#                             yeniSifre = input("Yeni şifrenizi giriniz:")
+#                             sifrem = yeniSifre
+#                             print(f"Şifreniz değiştirildi. Yeni şifreniz {sifrem}")
+#                             break
+#                         else:
+#                             print("Hatalı şifre")
+#                 elif islem == 5:
+#                     print(f"Mevcut bakiyeniz: {bakiye}₺")
+#                 elif islem == 6:
+#                     print(f"Borcunuz : {borc}₺")
+#                 elif islem == 7:
+#                     print("Çıkış yappılıyor...")
+#                     break
+#         elif hak == 1:
+#             print("Hakkınız dolmuştur.")
+#             break                
+# atm()
+
+
+# ! DICTIONARY KAVRAMI
+karakter = {
+    "isim":"Furkan",
+    "soyisim":"İlaslan",
+    "yas":26,
+    "meslek":"Eğitmen",
+}
+
+# print(karakter)
+# print(type(karakter))
+# print(len(karakter))
+# print(karakter["isim"])
+# karakter["medeni durum"] = "bekar"
+# print(karakter)
+# karakter["isim"] = "Yusuf"
+# print(karakter)
+# print(karakter.keys())
+# print(karakter.values())
+# print(karakter.items())
---
+++ DERSLER/ders6.py
@@ -0,0 +1,151 @@
+# ! Dictionary 
+
+karakter = {
+    "isim":"Furkan",
+    "soyisim":"İlaslan",
+    "yas":26,
+    "meslek":"Eğitmen",
+}
+
+# print(karakter)
+# print(type(karakter))
+# print(len(karakter))
+# print(karakter["isim"])
+# karakter["medeni durum"] = "bekar"
+# print(karakter)
+# karakter["isim"] = "Ahmet"
+# print(karakter)
+# print(karakter.keys())
+# print(karakter.values())
+# print(karakter.items())
+
+# ! Hem key hem de value değerini güncellemek için
+# karakter.update({"isim": "Ali" , "kan grubu": "A Rh+"})
+# print(karakter)
+
+# ! İstediğim elemanı silmek için
+# karakter.pop("meslek")
+# print(karakter)
+
+# ! Dictionary İle Döngü Kullanımı
+
+# for i in karakter:
+#     print(i)
+# key değerlerini döndürürü
+
+# for i in karakter:
+#     print(karakter[i])
+# value değerlerini döndürür.
+
+# for i,j in karakter.items():
+#     print(i,j)
+# Hem key hem de value değerlerini döndürür.
+
+
+# ! Örnek-1
+# ! Aşağıda verilen notlardan 50 ve üzeri olanları geçti diğerlerini kaldı olarak belirleyelim.
+
+# notlar = {"Ahmet": 30, "Selim": 58, "Hüseyin": 75, "Hasan":44}
+
+# print(notlar.items())
+
+# for ad,puan in notlar.items():
+#     if puan > 50:
+#         print(f"Öğrenci Adı: {ad}, Öğrenci Puanı : {puan} GEÇTİ")
+#     else:
+#         print(f"Öğrenci Adı: {ad}, Öğrenci Puanı : {puan} KALDI")
+        
+
+# ! Örnek-2
+# kelime_sozlugu = {
+#     "hello": "Merhaba",
+#     "book" : "Kitap",
+#     "read" : "Okumak",
+#     "good night" : "İyi geceler",
+# }
+
+# kelime = input("Bir kelime giriniz :").lower()
+
+# while True:
+#     if kelime in kelime_sozlugu:
+#         print(f"{kelime} : {kelime_sozlugu[kelime]} ")
+#         break
+#     else:
+#         print("Bu kelimenin sözlükte karşılığı yok. Kontrol ederek tekrar yazınız.")
+#         kelime = input("Başka bir kelime giriniz :").lower()
+
+
+# ! Örnek-3
+
+# ogrenci_notları = {}
+
+# while True:
+#     ogrenci_ad = input("Öğrenci adını giriniz (Çıkmak için q tuşuna basınız :)")
+
+#     if ogrenci_ad == "q":
+#         break
+
+#     ogrenci_notu = int(input("Notunuzu giriniz:"))
+#     ogrenci_notları[ogrenci_ad] = ogrenci_notu
+
+# aranan_ogrenci = input("Notunu görmek istediğiniz öğrencinin adını giriniz:")
+# if aranan_ogrenci in ogrenci_notları:
+#     print(f"{aranan_ogrenci}'nin notu : {ogrenci_notları[aranan_ogrenci]}")
+#     ogrenci_listesi = list(ogrenci_notları.items())
+#     print(ogrenci_listesi)
+# else:
+#     print("Öğrenci kayıtlı değildir.")
+
+
+# ! Örnek-4
+
+# veri = {"Ahmet": 30 , "Mehmet": 40, "Kerem": 20}
+
+# veri_listesi = list(veri.keys())
+# print(veri_listesi)
+
+
+# ! Örnek-5
+envanter = {}
+
+menu = """
+1 Ürün Eklemek
+2 ürün Çıkarmak
+3 Envanteri Listelemek
+4 Çıkış
+"""
+
+while True:
+    print(menu)
+
+    secim = input("Yapmak istediğiniz işlemi seçiniz: ")
+
+    if secim == "1":
+        urun = input("Eklemek istediğiniz ürünü giriniz: ")
+        miktar = int(input(f"Eklenecek {urun} miktarını giriniz : "))
+
+        if urun in envanter:
+            envanter[urun] += miktar
+        else:
+            envanter[urun] = miktar
+    
+    elif secim == "2":
+        urun = input("Çıkarmak istediğiniz ürünü giriniz: ")
+        miktar = int(input(f"Çıkarılacak {urun} miktarını giriniz : "))
+
+        if urun in envanter:
+            if envanter[urun] >= miktar:
+               envanter[urun] -= miktar 
+               print(f"{miktar} adet {urun} envanterden çıkarıldı.")
+            else:
+                print(f"Envanterde yeterli {urun} yok.")
+    elif secim == "3":
+        print("Güncel Envanter Durumu")
+        for key , value  in envanter.items():
+            print(f"{key} : {value} adet vardır.")
+
+    elif secim == "4":
+        break 
+    else:
+        print("Yanlış değer girdiniz")
+        
